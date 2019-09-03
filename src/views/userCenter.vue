97ad2fec85331f98baa4ba1cebfe608cdaeb072d
hongqi <2503389490@qq.com>
2019-09-03 16:45:47 +0800

    nothing

--- src/views/userCenter.vue
+++ src/views/userCenter.vue
@@ -23,7 +23,16 @@
         </div>
 
         <div class="footerCard">
-
+            <el-card class="foot-card">
+                <div class="total">
+                    <div class="num">本学期购书合计:</div>
+                    <el-button type="primary">确认提交</el-button>
+                </div>
+                <div class="buttons">
+                    <div class="balance">预存余额:</div>
+                    <el-button  type="info">历史购书查询</el-button>
+                </div>
+            </el-card>
         </div>
 
     </div>
@@ -47,6 +56,7 @@
 <style>
     .el-card__body{
         padding:14px;
+        width:95%;
     }
 </style>
 
@@ -63,7 +73,7 @@
         border: .05rem solid #D3D3D3;
         border-radius: .6em;
         box-shadow: 0 .05em .25em 	#D3D3D3;
-        justify-content: center;
+
         .box-card{
             font-size:.9rem;
             font-weight:600;
@@ -90,10 +100,10 @@
                         width:30%;
                     }
                     .stu-maj{
-                        width:45%;
+                        width:50%;
                     }
                     .stu-cla{
-                        width:25%;
+                        width:20%;
                     }
             }
         }
@@ -102,7 +112,7 @@
         height:1000px;
         width:80%;
         margin-top: 4.6rem;
-        margin-bottom:5rem;
+        margin-bottom:6.6rem;
         border: .1rem solid 	#D3D3D3;
         border-radius: .2em;
         background: blue;
@@ -111,10 +121,36 @@
     .footerCard{
         position: fixed;
         bottom:0;
-        height:5rem;
+        height:6.5rem;
         width:100%;
-        border-radius: .4em;
         background:black;
+        border: .05rem solid #D3D3D3;
+        border-radius: .6em;
+        box-shadow: 0 .05em .25em 	#D3D3D3;
+
+        .foot-card{
+            display:flex;
+            flex-direction: row ;
+            flex-wrap: nowrap;
+            justify-content: center;
+            align-items: center;
+            .total{
+                display:flex;
+                width:100%;
+                padding:.1rem 0 0 0 ;
+                .num{
+                    width:80%;
+                }
+            }
+            .buttons{
+                margin-top:.1rem;
+                display:flex;
+                .balance{
+                    width:70%;
+                }
+            }
+
+        }
     }
 
 

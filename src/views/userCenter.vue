7dba279d7323e0195f5a0175b3711d4f287765f4
hongqi <2503389490@qq.com>
2019-09-03 16:15:10 +0800

    user center

--- src/views/userCenter.vue
+++ src/views/userCenter.vue
@@ -1,13 +1,122 @@
 <template>
-    <div>用户中心</div>
+    <div class="studentInfo">
+        <div class="headerCard">
+            <el-card class="header box-card">
+
+
+                <div class="name">
+                   <div class="Stu-num"> <span>学号:{{ studentNumber }}</span></div>
+                   <div class="stu-name"> <span>姓名:{{ studentName }}</span></div>
+                </div>
+                <div class="grade">
+                    <div class="stu-gra"><span>年级:{{ grade }}</span></div>
+                    <div class="stu-maj"><span>专业:{{ major }}</span></div>
+                    <div class="stu-cla"><span>班级:{{ stuClass }}</span></div>
+                </div>
+
+
+            </el-card>
+        </div>
+
+        <div class="contentCard">
+
+        </div>
+
+        <div class="footerCard">
+
+        </div>
+
+    </div>
 </template>
 
 <script>
     export default {
-        name: "userCenter"
+        data(){
+            return {
+                studentNumber:'16110032',
+                studentName:'浦同宇',
+                grade:'2016',
+                major:'计算机科学',
+                stuClass:'1班',
+
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
+    .el-card__body{
+        padding:14px;
+    }
+</style>
 
+<style lang="scss" scoped>
+.studentInfo{
+    display:flex;
+    justify-content: center;
+    overflow: hidden;
+    .headerCard{
+        top:5.2rem;
+        width:100%;
+
+        position: fixed;
+        border: .05rem solid #D3D3D3;
+        border-radius: .6em;
+        box-shadow: 0 .05em .25em 	#D3D3D3;
+        justify-content: center;
+        .box-card{
+            font-size:.9rem;
+            font-weight:600;
+            display:flex;
+            flex-direction:column;
+
+            .name{
+                display:flex;
+                flex-direction: row ;
+                flex-wrap: nowrap;
+                margin-bottom: .4rem;
+                .Stu-num{
+                    width:50%;
+                }
+                .Stu-name{
+                    width:50%;
+                }
+            }
+            .grade{
+                display:flex;
+                flex-direction: row ;
+                flex-wrap: nowrap;
+                    .stu-gra{
+                        width:30%;
+                    }
+                    .stu-maj{
+                        width:45%;
+                    }
+                    .stu-cla{
+                        width:25%;
+                    }
+            }
+        }
+    }
+    .contentCard{
+        height:1000px;
+        width:80%;
+        margin-top: 4.6rem;
+        margin-bottom:5rem;
+        border: .1rem solid 	#D3D3D3;
+        border-radius: .2em;
+        background: blue;
+        box-shadow: 0 .05em .25em 	#D3D3D3;
+    }
+    .footerCard{
+        position: fixed;
+        bottom:0;
+        height:5rem;
+        width:100%;
+        border-radius: .4em;
+        background:black;
+    }
+
+
+}
 </style>
